9cc3c69f07b0866647710e0ed1c086b3b27cb23e
Eric Lau <ericglau@outlook.com>
2025-10-01 17:03:01 -0400

    Add context aware prompt example

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,7 +1,14 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
+import { completable } from "@modelcontextprotocol/sdk/server/completable.js";
 import { z } from "zod";
 
 export function registerPrompts(server: McpServer) {
+  registerBasicPrompt(server);
+  registerBasicWithTargetPrompt(server);
+}
+
+// Example of a basic prompt
+function registerBasicPrompt(server: McpServer) {
   server.registerPrompt(
     "basic",
     {
@@ -17,7 +24,42 @@ export function registerPrompts(server: McpServer) {
         content: {
           type: "text",
           text: `\
-Create unit tests for the file or folder at the following path:\n\n${fileOrFolder}`
+Create unit tests for the file or folder at the following path: \`${fileOrFolder}\``
+        }
+      }]
+    })
+  );
+}
+
+// Example of a context-aware prompt
+function registerBasicWithTargetPrompt(server: McpServer) {
+  server.registerPrompt(
+    "basic-with-target",
+    {
+      title: "Unit Tests with Target",
+      description: "Generate unit tests for the code at the provided locations, to the specified file or folder.",
+      argsSchema: {
+        fileOrFolder: z.string().describe("The file or folder path to generate unit tests for."),
+        targetFileOrFolder: completable(z.string(), (_value, context) => {
+          const fileOrFolder = context?.arguments?.["fileOrFolder"];
+          if (fileOrFolder) {
+            if (fileOrFolder.endsWith(".ts")) {
+              return [`${fileOrFolder.slice(0, -3)}.test.ts`];
+            }
+          }
+          return [];
+        }).describe("Where to write the unit tests."),
+      }
+    },
+    ({ fileOrFolder, targetFileOrFolder }) => ({
+      messages: [{
+        role: "user",
+        content: {
+          type: "text",
+          text: `\
+Create unit tests for the file or folder at the following path: \`${fileOrFolder}\`
+
+Write the unit tests to the following path: \`${targetFileOrFolder}\``
         }
       }]
     })
